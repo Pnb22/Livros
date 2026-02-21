64d20204e725c9e2aca5fe6bdfa3c98d29c07fb7
Arthur Fonseca <arthurfgp22@gmail.com>
2026-02-21 15:13:22 -0300

    Update API URL for book service

--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { Book } from "../types/Book";
 
-const API_URL = "https://crudcrud.com/api/289efd14ff294b8b92de981d42a34217/books";
-
+const API_URL = "https://crudcrud.com/api/c970be1ead8f4f97a792abae25701603/books";
 export const getBooks = () => axios.get<Book[]>(API_URL);
 
 export const addBook = (book: Book) => axios.post(API_URL, book);
