848adcfde2d0eb973119b047b0dbe19572670ca0
Arthur Fonseca <arthurfgp22@gmail.com>
2026-02-21 14:49:59 -0300

    feat: atualizar BookList para passar onToggleStatus e mensagem vazia

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,17 +1,26 @@
-
 import { Book } from "../types/Book";
 import BookItem from "./BookItem";
 
 interface Props {
   books: Book[];
   onDelete: (id: string) => void;
+  onToggleStatus: (id: string, currentStatus: "Lido" | "Não lido") => void;
 }
 
-export default function BookList({ books, onDelete }: Props) {
+export default function BookList({ books, onDelete, onToggleStatus }: Props) {
+  if (books.length === 0) {
+    return <p>Nenhum livro cadastrado ainda.</p>;
+  }
+
   return (
     <ul>
       {books.map((book) => (
-        <BookItem key={book._id} book={book} onDelete={onDelete} />
+        <BookItem
+          key={book._id}
+          book={book}
+          onDelete={onDelete}
+          onToggleStatus={onToggleStatus}
+        />
       ))}
     </ul>
   );
