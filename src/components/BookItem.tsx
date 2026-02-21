cd4a685b45ff23b52906642ccf50c764789746c8
Arthur Fonseca <arthurfgp22@gmail.com>
2026-02-21 14:49:25 -0300

    Update BookItem.tsx

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,15 +1,19 @@
-
 import { Book } from "../types/Book";
 
 interface Props {
   book: Book;
   onDelete: (id: string) => void;
+  onToggleStatus: (id: string, currentStatus: "Lido" | "Não lido") => void;
 }
 
-export default function BookItem({ book, onDelete }: Props) {
+export default function BookItem({ book, onDelete, onToggleStatus }: Props) {
   return (
     <li>
-      {book.title} - {book.author} ({book.status})
+      <strong>{book.title}</strong> - {book.author}{" "}
+      <em>({book.status})</em>
+      <button onClick={() => onToggleStatus(book._id!, book.status)}>
+        {book.status === "Lido" ? "Marcar como Não lido" : "Marcar como Lido"}
+      </button>
       <button onClick={() => onDelete(book._id!)}>Remover</button>
     </li>
   );
