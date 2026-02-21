8fcac3ecc6314294726cde0167cf3e07c4ebcd92
Arthur Fonseca <arthurfgp22@gmail.com>
2026-02-21 14:48:10 -0300

    fix: corrigir URL da API e adicionar updateBook no bookService

--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -1,8 +1,7 @@
-
 import axios from "axios";
 import { Book } from "../types/Book";
 
-const API_URL = "https://crudcrud.com/api/289efd14ff294b8b92de981d42a34217/books ";
+const API_URL = "https://crudcrud.com/api/289efd14ff294b8b92de981d42a34217/books";
 
 export const getBooks = () => axios.get<Book[]>(API_URL);
 
@@ -10,3 +9,6 @@ export const addBook = (book: Book) => axios.post(API_URL, book);
 
 export const deleteBook = (id: string) =>
   axios.delete(`${API_URL}/${id}`);
+
+export const updateBook = (id: string, book: Omit<Book, "_id">) =>
+  axios.put(`${API_URL}/${id}`, book);
